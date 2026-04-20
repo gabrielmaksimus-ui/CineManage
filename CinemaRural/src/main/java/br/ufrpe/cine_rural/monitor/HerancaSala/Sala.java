2b4141dc16e07b64959e8e117d84d95092a5c845
Maksimus <gabriel.maksimus@ufrpe.br>
2026-04-20 11:33:18 -0300

    Tirando Abstract

--- CinemaRural/src/main/java/br/ufrpe/cine_rural/monitor/HerancaSala/Sala.java
+++ CinemaRural/src/main/java/br/ufrpe/cine_rural/monitor/HerancaSala/Sala.java
@@ -1,9 +1,9 @@
 package br.ufrpe.cine_rural.monitor.HerancaSala;
 
-public abstract class Sala {
-    protected final int id;
-    protected final int capacidade;
-    protected final double preco;
+public class Sala {
+    protected int id;
+    protected int capacidade;
+    protected double preco;
 
     public Sala(int id, int capacidade, double preco){
         this.id = id;
